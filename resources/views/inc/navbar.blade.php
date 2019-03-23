d418a3d5b7d7e0c2da4e156ed7be3e66407484d9
yash sharma <sharmayash44ys@gmail.com>
2019-03-23 15:31:42 +0530

    added posts

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -8,6 +8,9 @@
         <li class="nav-item">
           <a class="nav-link" href="/services">Services</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/posts">Posts</a>
+        </li>
       </ul>
     </div>
 </nav>
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>posts</h1>
+    @if(count($posts) > 0)
+        @foreach($posts as $post)
+            <div class="well">
+                <h3><a href="/posts/{{$post->id}}">{{$post->title}}</a></h3>
+                <small>Added On {{$post->created_at}}</small>
+            </div>
+        @endforeach
+        {{$posts->links()}}
+    @else
+        <p>No Post Found</p>
+    @endif
+@endsection
